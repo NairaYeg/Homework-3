784a92a4b2638ba1ec75f40d31ba33318107e6be
Naira-753 <60625888+Naira-753@users.noreply.github.com>
2020-05-23 20:58:55 +0400

    Create Object.js

---
+++ Object.js
@@ -0,0 +1,15 @@
+// return the obj size
+const retSizeOfObj = (obj) => {
+  let arr = [];
+  for (let key in obj) {
+    arr.push(obj[key]);
+  }
+  if (arr.length == 0) {
+    return "Empty";
+  }
+
+  return arr.length;
+};
+
+console.log(retSizeOfObj({})); // "Empty" ;
+console.log(retSizeOfObj({ a: 1, b: 5, c: "hjsdh" })); // 3
